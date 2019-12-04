d19cd5c837b9a002432b7985963f2fd648d0cd6d
Kelvin Koh <kelvin.koh@condecosoftware.com>
2019-12-04 11:19:42 +0000

    v1.1 with working portrait and landscape mode with transition animation

--- app/src/main/java/com/example/android/rssreader/DetailFragment.java
+++ app/src/main/java/com/example/android/rssreader/DetailFragment.java
@@ -8,13 +8,23 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class DetailFragment extends Fragment {
-
+    public static final String EXTRA_TEXT ="text";
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_rssitem_detail,
                 container, false);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            String text = bundle.getString(EXTRA_TEXT);
+            setText(text);
+        }
     }
 
     public void setText(String text) {
